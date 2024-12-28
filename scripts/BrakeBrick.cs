88898a981c7a5a11cc3f3b81329369dd256faf05
miorezu <m.miorezu@gmail.com>
2024-12-28 15:24:49 +0200

    Add color

--- scripts/BrakeBrick.cs
+++ scripts/BrakeBrick.cs
@@ -3,9 +3,25 @@ using System;
 
 public partial class BrakeBrick : StaticBody2D
 {
+    private Color[] Colors =
+    {
+        new Color(1, 0, 0),
+        new Color(1, 1, 0),
+        new Color(0, 1, 0),
+        new Color(0, 0, 1),
+        new Color(1, 0.75f, 0.8f)
+    };
+
     [Export] private int _hitCount = 1;
     [Export] private Sprite2D _sprite;
     [Export] private CollisionShape2D _collisionShape;
+
+    private Color GetRandomColor()
+    {
+        Random random = new Random();
+        int index = random.Next(Colors.Length);
+        return Colors[index];
+    }
 
     public void Hit()
     {
@@ -23,5 +39,4 @@ public partial class BrakeBrick : StaticBody2D
             QueueFree();
         }
     }
-
 }
