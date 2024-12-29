f3ded61e74a048501aaae87bffd674bcb38fb0ca
fade <dimabreusgo@gmail.com>
2024-12-29 17:32:06 +0200

    Player hp added

---
+++ scripts/PlayerHp.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class PlayerHp : Control
+{
+	public int HP { get; set; } = 3;
+	[Export] private AnimatedSprite2D _sprite;
+	public override void _Ready()
+	{
+		_sprite.Frame = 0;
+	}
+
+	public bool TryKillPlayer()
+	{
+		HP--;
+		UpdateView();
+		return HP <= 0;
+	}
+
+	public void UpdateView()
+	{
+		_sprite.Frame++;		
+	}
+}
