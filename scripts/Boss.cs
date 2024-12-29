c79842dc14d30603fe205165b9a29fa67022519f
fade <dimabreusgo@gmail.com>
2024-12-29 18:37:57 +0200

    boss rage mode

--- scripts/Boss.cs
+++ scripts/Boss.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections;
 using BrickBraker.scenes;
 using BrickBraker.scripts;
 
@@ -8,18 +9,17 @@ public partial class Boss : CharacterBody2D, IBreakable
     [Export] public int Speed { get; set; }
     [Export] public BossGun[] Guns;
     [Export] public int HP { get; set; } = 10;
+    private int currentHP;
 
     public override void _Ready()
     {
+        currentHP = HP;
         Velocity = Vector2.Right * Speed;
     }
 
     public void Shoot()
     {
-        foreach (var gun in Guns)
-        {
-            gun.Shoot();
-        }
+        Guns[GD.Randi() % Guns.Length].Shoot();
     }
     public override void _PhysicsProcess(double delta)
     {
@@ -33,10 +33,19 @@ public partial class Boss : CharacterBody2D, IBreakable
 
     public void Hit()
     {
-        HP--;
+        currentHP--;
+        if(currentHP == HP /2) RageMode();
+        if (currentHP <= 0) QueueFree();
     }
 
+    private void RageMode()
+    {
+        GetNode<AnimatedSprite2D>("s").Play("Rage");
+        Speed *= 2;
+        GetNode<Timer>("Timer").WaitTime /= 2;
+    }
     public void ApplyEffect(Ball ball)
     {
+        Game.Instance.DeleteBall(ball);
     }
 }
--- scripts/BossGun.cs
+++ scripts/BossGun.cs
@@ -6,5 +6,8 @@ public partial class BossGun : Node2D
     public void Shoot()
     {
         GetNode<CpuParticles2D>("s").Emitting = true;
+        var randomSpread = (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+        Game.Instance.CreateBall(GlobalPosition, new Vector2(0, 1).Rotated(randomSpread));
+
     }
 }
