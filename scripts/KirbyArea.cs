e1dbb48911af96d4217544200cee2a52e5b416ef
fade <dimabreusgo@gmail.com>
2025-01-02 23:31:27 +0200

    kirby chance and export

--- scripts/KirbyArea.cs
+++ scripts/KirbyArea.cs
@@ -4,23 +4,26 @@ using BrickBraker.scenes;
 
 public partial class KirbyArea : Node2D
 {
+    [Export(PropertyHint.Range, "0,1,0.01")]
+    private float _kirbyChance;
 
-	[Export] private PackedScene _kirby;
-	private Vector2 _position;
+    [Export] private PackedScene _kirby;
+    private Vector2 _position;
 
-	private void OnBodyEntered(Node2D body)
-	{
-		if (body is Ball ball)
-		{
-			var position = ball.GlobalPosition;
-			
-			var kirby = _kirby.Instantiate<Kirby>();
-			AddChild(kirby);
-			kirby.GlobalPosition = position;
-			kirby.Rotation = 0;
-			kirby.Scale = new Vector2(3, 3);
-			Game.Instance.DeleteBall(ball);
-		}
-	}
-
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is Ball ball)
+        {
+            if (GD.Randf() <= _kirbyChance)
+            {
+                var position = ball.GlobalPosition;
+                var kirby = _kirby.Instantiate<Kirby>();
+                AddChild(kirby);
+                kirby.GlobalPosition = position;
+                kirby.Rotation = 0;
+                kirby.Scale = new Vector2(3, 3);
+                Game.Instance.DeleteBall(ball);
+            }
+        }
+    }
 }
